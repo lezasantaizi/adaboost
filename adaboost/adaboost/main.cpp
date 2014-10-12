5f438610a55f29b68d6cbc996ee628560e778f8f
rxm24217 <xiaomingx.ren@intel.com>
2014-10-12 23:42:01 +0800

    easy

--- adaboost/adaboost/main.cpp
+++ adaboost/adaboost/main.cpp
@@ -1,4 +1,30 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void stumpClassify(vector<float>& retArray, vector<vector<float>>& dataMatrix, int dimen, int threshVal, string threshIneq)
+{
+	for (int i = 0 ; dataMatrix.size(); i++)
+	{
+		retArray.push_back(1);
+	}
+	if (threshIneq == "lt")
+	{
+		for (int i = 0 ;i < dataMatrix.size(); i++)
+		{
+			retArray[dataMatrix[i][dimen] <= threshVal] = -1.0;
+		}
+	}
+	else
+	{
+		for (int i = 0 ;i < dataMatrix.size(); i++)
+		{
+			retArray[dataMatrix[i][dimen] > threshVal] = -1.0;
+		}
+	}
+}
 
 int main()
 {
