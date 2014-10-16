e21bcc68b6e06c35a58ffc18a16eae35dc409449
renxiaoming <xiaomingx.ren@intel.com>
2014-10-16 22:17:32 +0800

    finish?

--- adaboost/adaboost/main.cpp
+++ adaboost/adaboost/main.cpp
@@ -20,7 +20,7 @@ using namespace cv;
 
 void stumpClassify(vector<float>& retArray, vector<vector<float>>& dataMatrix, int dimen, float threshVal, int threshIneq)
 {
-	for (int i = 0 ; dataMatrix.size(); i++)
+	for (int i = 0 ; i< dataMatrix.size(); i++)
 	{
 		retArray.push_back(1);
 	}
@@ -66,7 +66,7 @@ void stumpClassify(vector<float>& retArray, vector<vector<float>>& dataMatrix, i
 //	return bestStump,minError,bestClasEst
 
 
-void buildStump(vector<vector<float>>& dataArr,vector<int>& classLabels,vector<float>& D,vector<float> bestStump,float& minError,vector<float> bestClasEst)
+void buildStump(vector<vector<float>>& dataArr,vector<int>& classLabels,vector<float>& D,vector<float> bestStump,float& minError,vector<float>& bestClasEst)
 {
 	int m = dataArr.size();
 	int n = dataArr[0].size();
@@ -101,7 +101,7 @@ void buildStump(vector<vector<float>>& dataArr,vector<int>& classLabels,vector<f
 			float threshVal = (tempMin + float(j) * stepSize);
 			vector<float> predictedVals;
 			vector<float> errArr ;
-			float weightedError;
+			float weightedError = 0;
 			for (int ineq = 0; ineq<2 ; ineq++)
 			{
 				stumpClassify(predictedVals,dataArr,i,threshVal,ineq);
@@ -148,18 +148,18 @@ void buildStump(vector<vector<float>>& dataArr,vector<int>& classLabels,vector<f
 //	errorRate = aggErrors.sum()/m
 //	print "total error: ",errorRate
 //	if errorRate == 0.0: break
-
-void adaBoostTrainDS(vector<vector<float>>dataArr,vector<int>classLabels, int numIt=40)
+//return weakClassArr,aggClassEst
+void adaBoostTrainDS(vector<vector<float>>& dataArr,vector<int>& classLabels,vector<vector<float>>& weakClassArr,vector<float>& aggClassEst, int numIt=40)
 {
 	int m = dataArr.size();
 	vector<float> D;
-	vector<float> aggClassEst;
 	float error;
 	vector<float> bestStump;
 	vector<float> bestClasEst;
+	
 	for (int i = 0 ;i< m ;i++)
 	{
-		D.push_back(1/m);
+		D.push_back(1.0/m);
 		aggClassEst.push_back(0);
 	}
 
@@ -167,16 +167,51 @@ void adaBoostTrainDS(vector<vector<float>>dataArr,vector<int>classLabels, int nu
 	{
 		buildStump(dataArr, classLabels, D, bestStump, error, bestClasEst);
 		float alpha = 0.5*log((1.0-error)/MAX(error,1e-16));
+		bestStump.push_back(alpha);
+		weakClassArr.push_back(bestStump) ;
+		float sum_D = 0;
+		float sum_E = 0;
+		for (int j =0 ;j < classLabels.size();j++)
+		{
+			//vector<float> expon;
+			float temp = -1*alpha*classLabels[j]*bestClasEst[j];
+			//expon.push_back(temp)  ;
+			D[j] = D[j] * exp(temp);
+			sum_D += D[j];
+			aggClassEst[j] += alpha * bestClasEst[j];
+		}
+		for (int j = 0 ;j<classLabels.size();j++)
+		{
+			//vector<int> aggErrors;
+			D[j] /= sum_D;
+			//aggErrors.push_back(aggClassEst[j]>0?1:-1 != classLabels[j]);
+			sum_E += aggClassEst[j]>0?1:-1 != classLabels[j];
+		}
+		//float errorRate = ;
+		if(sum_E/m <=1e-4)
+			break;
 
 	}
 
 }
 
-void loadSimpData()
+void loadSimpData(vector<vector<float>>& datMat,vector<int>& clsLabels)
 {
 	float datMatArray[] = {1.0,2.1,2.0,1.1,1.3,1.0,1.0,1.0,2.0,1.0};
+	
 	int classLabels[] = {1,1,-1,-1,1};
-	Mat datMat(5, 2, CV_32FC1, datMatArray);
+
+	for (int i = 0 ;i < 5; i++)
+	{
+		vector<float> temp;
+		for (int j = 0 ;j<2;j++)
+		{
+			temp.push_back( datMatArray[i*2 + j] );
+		}
+		clsLabels.push_back(classLabels[i]);
+		datMat.push_back(temp);
+	}
+	//Mat datMat(5, 2, CV_32FC1, datMatArray);
 
 }
 
@@ -189,5 +224,11 @@ void loadDataSet(string fileName)
 }
 int main(int argc, char** argv)
 {
+	vector<vector<float>> data;
+	vector<int> labels;
+	vector<vector<float>> weakClassArr;
+	vector<float> aggClassEst;
+	loadSimpData(data,labels);
+	adaBoostTrainDS(data,labels,weakClassArr,aggClassEst);
 	return 0;
 }
